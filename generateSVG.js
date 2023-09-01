af97192783e7efc9d35d54734f7ff18fc2586930
evan-pieper <evanjpieper@gmail.com>
2023-09-01 13:35:53 -0700

    added inquirer questions, validated all input except for color, added generateSVG.js file

---
+++ generateSVG.js
@@ -0,0 +1,15 @@
+function generateSVG (data){  //takes in data and returns an SVG string based on the data
+    return false; //TODO: implement this function
+}
+
+module.exports = generateSVG;
+
+
+// Example SVG
+/*<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+<circle cx="150" cy="100" r="80" fill="green" />
+
+<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+
+</svg> */
--- index.js
+++ index.js
@@ -1,3 +1,72 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateSVG = require('./generateSVG.js');
 
+function validateInput(input) {
+    if (input === '') {
+        return 'Please enter a valid input.';
+    }
+    return true;
+}
+
+const questions = [
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to 3 characters for your logo: ',
+        validate: function (input) {
+            if(input.length < 1) {
+                return 'Please enter at least 1 character.';
+            }
+            if (input.length > 3) {
+                return 'Please enter up to 3 characters.';
+            }
+            return true;
+        },
+    },
+
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What color would you like your logo to be?',
+        validate: function (input) {
+            if(input.length < 1) {
+                return 'Please enter a color.';
+            }
+            return true;
+        },
+    },
+
+    {
+        type: 'list',
+        name: 'title',
+        message: 'What shape would you like your logo to be?',
+        choices: [
+            "Circle",
+            "Triangle",
+            "Square",
+        ],
+        validate: validateInput,
+    },
+];
+
+function writeToFile(fileName, data)  //takes in a file name and data, then writes the data to the file (from fileName)
+{
+    fs.writeFile(fileName, generateSVG(data), function (err) {
+        if (err) {
+            console.log("error writing to file");
+            return console.log(err);
+        }
+    });
+}
+
+
+function init() { // takes in the questions array, then uses inquirer to prompt the user for answers to the questions, then passes the answers to generateMarkdown.js, which returns the markdown, which is then passed to writeToFile
+    inquirer.prompt(questions).then((data) => {
+        //data.license = getLicense(data.license);
+        writeToFile("./MdGenerator_output/README.md", data);
+    });
+}
+
+// Function call to initialize app
+init();
